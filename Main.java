a03cd6009b0309bc5740e9befbd4df534592b065
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2025-02-26 23:59:18 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner q = new Scanner(System.in);
+        int a = q.nextInt();
+        int b = 0;
+        int c;
+        for (int i = 2; i < a; i++) {
+            if (a % i == 0)
+                b++;
+        }
+
+        if (b > 0) {
+            System.out.println("not prime");
+        } else {
+            System.out.println("Is prime");
+
+        }
+    }
+}
