a2fac47256e0f734b38bc769f274c630954407a8
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2026-02-20 14:27:44 +0530

    Fix formatting and clean up triangle.java

--- triangle.java
+++ triangle.java
@@ -4,6 +4,7 @@ class triangle {
     public static void main(String[] args) {
         Scanner q = new Scanner(System.in);
         int a = q.nextInt();
+        
         int b = q.nextInt();
         int c = q.nextInt();
         if ((a + b) > c && (b + c) > a && (c + a) > b) {
@@ -15,5 +16,6 @@ class triangle {
     }
 
 }
+
 
 
