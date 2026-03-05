270794b3de814a39066433f52b610faba1b8da3d
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2026-03-06 00:02:21 +0530

    Update primenumber.java

--- primenumber.java
+++ primenumber.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class primenumber {
     public static void main(String[] args) {
         Scanner q = new Scanner(System.in);
+        
         int a = q.nextInt();
         int b = 0;
         int c;
@@ -18,6 +19,7 @@ public class primenumber {
         }
     }
 }
+
 
 
 
