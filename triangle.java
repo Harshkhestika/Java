aec47080767ffd68aaead8e5b03f26ef98da899e
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2026-03-11 23:15:49 +0530

    Update triangle.java

--- triangle.java
+++ triangle.java
@@ -5,6 +5,7 @@ class triangle {
 
         
         Scanner q = new Scanner(System.in);
+        
         int a = q.nextInt();
         int b = q.nextInt();
         int c = q.nextInt();
@@ -16,6 +17,7 @@ class triangle {
         }
     }
 }
+
 
 
 
