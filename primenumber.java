ed994e33f680593f58faaf0e3f0fb82089a51384
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2026-03-12 22:25:04 +0530

    Update primenumber.java

--- primenumber.java
+++ primenumber.java
@@ -6,6 +6,7 @@ public class primenumber {
         int a = q.nextInt();
         int b = 0;
         int c;
+        
         for (int i = 2; i < a; i++) {
             if (a % i == 0)
                 b++;
@@ -19,6 +20,7 @@ public class primenumber {
         
     }
 }
+
 
 
 
