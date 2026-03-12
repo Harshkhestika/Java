4d51a48e3fb8cf0c6c18570206694d7a19575d2e
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2026-03-12 22:27:02 +0530

    Update primenumber.java

--- primenumber.java
+++ primenumber.java
@@ -1,18 +1,14 @@
 import java.util.*;
 public class primenumber {
     public static void main(String[] args) {
-        
         Scanner q = new Scanner(System.in);
         int a = q.nextInt();
         int b = 0;
         int c;
-
         for (int i = 2; i < a; i++) {
             if (a % i == 0)
                 b++;
         }
-
-        
         if (b > 0) {
             System.out.println("not prime");
         } else {
@@ -22,6 +18,7 @@ public class primenumber {
         
     }
 }
+
 
 
 
