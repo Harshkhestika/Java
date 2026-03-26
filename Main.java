24f9cae164a9081068ba4b47f99fc221e67fbf12
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2026-03-26 10:36:31 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,7 +13,7 @@ public class Main {
         }
 
         if (b > 0) {
-            System.out.println("not prime");
+            System.out.println("not prime") ;
         } else {
             System.out.println("Is prime");
 
