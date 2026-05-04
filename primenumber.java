3f04ce57816bb4ad316770f7b13175c804130c0f
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2026-05-04 23:45:51 +0530

    Update primenumber.java

--- primenumber.java
+++ primenumber.java
@@ -8,7 +8,7 @@ public class primenumber {
         for (int i = 2; i < a; i++) {
             if (a % i == 0)
                 b++;
-        }     
+        }      
         if (b > 0) {
             System.out.println("not prime");
         } else {
