282b118c000e49d0cd141b1422035abd7629af67
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2026-05-12 11:16:08 +0530

    Refactor triangle class structure and formatting

--- triangle.java
+++ triangle.java
@@ -1,5 +1,6 @@
 import java.util.*;  
-class triangle {
+class triangle { 
+    
     public static void main(String[] args) {     
         Scanner q = new Scanner(System.in);
         int a = q.nextInt();
