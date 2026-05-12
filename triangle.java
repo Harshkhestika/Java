6a3008d26bdaa1dd0d56f58e5a5fe57568f217ff
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2026-05-12 11:17:04 +0530

    Update triangle.java

--- triangle.java
+++ triangle.java
@@ -8,7 +8,7 @@ class triangle {
         int c = q.nextInt();
 
         if ((a + b) > c && (b + c) > a && (c + a) > b)  {   
-            System.out.println("Valid triangle") ;
+            System.out.println("Valid triangle's") ;
         }         
         else {
             System.out.println("Not valid triangle's");
