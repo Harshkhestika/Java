6bc70c51a0818e70f8ab2b92f8d94f49eeb85f2a
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2026-05-12 11:17:28 +0530

    Update triangle.java

--- triangle.java
+++ triangle.java
@@ -11,7 +11,7 @@ class triangle {
             System.out.println("Valid triangle's") ;
         }         
         else {
-            System.out.println("Not valid triangle's") ;
+            System.out.println("Not valid triangle's")  ;
         }
     }
     
