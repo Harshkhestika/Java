5cb59b2951e1dcad96ef317a82a50f8499855699
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2026-05-12 11:16:31 +0530

    Refactor triangle class and main method

--- triangle.java
+++ triangle.java
@@ -11,7 +11,7 @@ class triangle {
             System.out.println("Valid triangle");
         }         
         else {
-            System.out.println("Not valid triangle's") ;
+            System.out.println("Not valid triangle's");
         }
     }
     
