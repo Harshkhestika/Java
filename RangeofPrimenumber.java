2c763cd935a079900aba27a7394e11fce49ed69d
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2026-05-29 22:51:15 +0530

    Update RangeofPrimenumber.java

--- RangeofPrimenumber.java
+++ RangeofPrimenumber.java
@@ -7,7 +7,7 @@ public class RangeofPrimenumber {
         System.out.println("Enter any random number: " + n);
         for (int i = 2; i <= n; i++) {
             int b = 0; 
-            for (int j = 2; j < i; j++) { 
+            for (int j = 2; j < i; j++) {   
                 if (i % j == 0) {  
                     b++;
                     break;   
