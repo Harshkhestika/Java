8ea303aed8eda3497967669b103722d227151b77
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2026-06-03 23:12:04 +0530

    Refactor prime number checking code

--- primenumber.java
+++ primenumber.java
@@ -9,7 +9,7 @@ public class primenumber  {
         for (int i = 2; i < a; i++) { 
             if (a % i == 0)   
                 b++; 
-        }          
+        }           
         if (b > 0) {
             System.out.println("not prime") ;
         } else {
