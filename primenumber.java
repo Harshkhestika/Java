13ca6cbdbe44cd08dedf245d06c4b726daf1790a
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2026-06-03 23:13:28 +0530

    Refactor prime number checking logic

--- primenumber.java
+++ primenumber.java
@@ -12,7 +12,7 @@ public class primenumber  {
         }           
         if (b > 0) {
             System.out.println("not prime") ;
-        } else {
+        } else { 
             System.out.println("Is prime") ;
         }
     } 
