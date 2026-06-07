d87ed57a65ad819151d3ad0c9d2598b68771ab3d
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2026-06-07 23:45:47 +0530

    Refactor prime number checking code

--- primenumber.java
+++ primenumber.java
@@ -4,7 +4,7 @@ public class primenumber  {
     public static void main(String[] args) {
         Scanner q = new Scanner(System.in) ; 
         int a = q.nextInt();
-        int b = 0;  
+        int b = 0;   
         int c ;    
         for (int i = 2; i < a; i++) { 
             if (a % i == 0)     
