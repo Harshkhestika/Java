909ab8952c4677d892af94ed65a952aa535d7247
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2026-06-24 10:13:20 +0530

    Update triangle.java

--- triangle.java
+++ triangle.java
@@ -10,7 +10,7 @@ class triangle {
         }              
         else {  
             System.out.println("Not valid triangle's"); 
-        }   
+        }    
                  
     }      
 }  
