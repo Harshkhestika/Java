6505288f63e9523d11014658600bc5c1528ecee9
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2026-06-29 23:07:10 +0530

    Refactor Main.java for prime number check

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner q = new Scanner(System.in);
         int a = q.nextInt(); 
-        int b = 0;
+        int b = 0; 
         int c;
         for (int i = 2; i < a; i++) {
             if (a % i == 0)
