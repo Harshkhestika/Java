1429bd7c1032c64cba82c3ed6c285cc2c74d0c01
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2026-07-14 11:07:08 +0530

    Format Main.java with consistent spacing and imports

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
         Scanner q = new Scanner(System.in);
         int a = q.nextInt(); 
         int b = 0; 
-        int c;
+        int c; 
         for (int i = 2; i < a; i++) { 
             if (a % i == 0)
                 b++;   //
